cff817d2a821748536cf36b5a671f512dbb96293
Ashiq1028 <ashiqoldvythiri@gmail.com>
2023-08-07 17:26:28 +0530

    first commit

---
+++ bin/darttutorials.dart
@@ -0,0 +1,5 @@
+import 'package:darttutorials/darttutorials.dart' as darttutorials;
+
+void main(List<String> arguments) {
+  print('Hello world: ${darttutorials.calculate()}!');
+}
---
+++ test/darttutorials_test.dart
@@ -0,0 +1,8 @@
+import 'package:darttutorials/darttutorials.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
