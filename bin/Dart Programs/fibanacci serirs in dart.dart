dee4bde01ae44253c718b944d38d23cc2498f8ff
Ashiq1028 <141636952+Ashiq1028@users.noreply.github.com>
2023-08-14 21:08:39 +0530

    Update fibanacci serirs .dart

--- bin/Dart Programs/fibanacci serirs in dart.dart
+++ bin/Dart Programs/fibanacci serirs in dart.dart
@@ -4,12 +4,17 @@
 
 
 
-class Captain{
-  void display (){
-    print('ASHIQ');
+void main() {
+  int n = 10;
+  print("Fibonacci Series:");
+  for (int i = 0; i < n; i++) {
+    print(fibonacci(i));
   }
 }
-void main(){
-  Captain obj = new Captain();
-  obj.display();
+
+int fibonacci(int n) {
+  if (n <= 1) {
+    return n;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
